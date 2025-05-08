d9f1e3e5ac678b1a3455b2022a20188cd8fa50c8
Minhyeok <jyd7549000@gmail.com>
2025-05-08 21:20:57 +0900

    test

---
+++ src/variable/Var8.java
@@ -0,0 +1,14 @@
+package variable;
+
+public class Var8 {
+    public static void main(String[] args) {
+        byte b = 127;
+        short s = 32767;
+        int i = 2147483647; // (약 20억);
+        long l = 99999L; // (아무튼 엄청 큼)
+
+        float f = 10.0f;
+        double d = 10.0;
+
+    }
+}
